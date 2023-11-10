6cb06a7ed56bbed0bc05f248f91882750b8674ef
Lucasl26 <lucas.condori12@gmail.com>
2023-11-10 13:14:53 -0300

    v2

--- app/src/main/java/com/example/proyectoblanco/Register.java
+++ app/src/main/java/com/example/proyectoblanco/Register.java
@@ -20,7 +20,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Register extends AppCompatActivity {
+public class
+Register extends AppCompatActivity {
 
     EditText editText1, editText2;
     TextView Textview1, Textview2;

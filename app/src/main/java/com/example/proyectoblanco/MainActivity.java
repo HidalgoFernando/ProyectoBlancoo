8bdbfb244a5348298627ca8e1fd7f4bb188d803f
julianwolker01 <julianbriceno455>
2023-11-17 15:49:19 -0300

    gfg

--- app/src/main/java/com/example/proyectoblanco/MainActivity.java
+++ app/src/main/java/com/example/proyectoblanco/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,9 +15,10 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-    TextView detalles;
+    TextView detalles, inicio;
     Button Deslog, vertarjeta, agregartarjeta, Lector;
     FirebaseUser usuario;
+    ImageView verperfil;
 
 
 
@@ -29,10 +31,20 @@ public class MainActivity extends AppCompatActivity {
         Deslog = findViewById(R.id.Deslogear);
         detalles = findViewById(R.id.detalles);
         usuario = auth.getCurrentUser();
+        verperfil = findViewById(R.id.imageView6);
         vertarjeta = findViewById(R.id.button2);
         agregartarjeta = findViewById(R.id.button3);
         Lector = findViewById(R.id.button6);
+        inicio = findViewById(R.id.Bienvenido);
 
+        verperfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getApplicationContext(), miperfil.class);
+                startActivity(intent2);
+                finish();
+            }
+        });
         Lector.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
         else {
+
+            inicio.setText("Bienvenido");
             detalles.setText(usuario.getEmail());
         }
 
---
+++ app/src/main/java/com/example/proyectoblanco/miperfil.java
@@ -0,0 +1,99 @@
+package com.example.proyectoblanco;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class miperfil extends AppCompatActivity {
+
+    Button cambiar, volver;
+    FirebaseAuth auth;
+    FirebaseUser usuario;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miperfil);
+
+        cambiar = findViewById(R.id.button9);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        usuario = auth.getCurrentUser();
+        volver = findViewById(R.id.button10);
+
+        String correoUsuario = usuario.getEmail();
+        buscarUsuarioPorCorreo(correoUsuario);
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void buscarUsuarioPorCorreo(final String correoUsuario) {
+        db.collection("usuarios")
+                .document(correoUsuario)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot usuarioDocument = task.getResult();
+                            if (usuarioDocument.exists()) {
+                                mostrarDatosUsuario(usuarioDocument);
+                            } else {
+                                mostrarMensaje("No se encontró el usuario con el correo asociado.");
+                            }
+                        } else {
+                            mostrarMensaje("Error al buscar el usuario por correo: " + task.getException().getMessage());
+                        }
+                    }
+                });
+    }
+    private void mostrarDatosUsuario(DocumentSnapshot usuarioDocument) {
+        String nombre = usuarioDocument.getString("nombre");
+        String apellido = usuarioDocument.getString("apellido");
+        int edad = usuarioDocument.getLong("edad").intValue();
+        String telefono = usuarioDocument.getString("telefono");
+        String correo = usuarioDocument.getId();
+
+        TextView nombreTextView = findViewById(R.id.nombreTextView2);
+        nombreTextView.setText("Nombre: " + nombre);
+
+        TextView apellidoTextView = findViewById(R.id.apellidoTextView2);
+        apellidoTextView.setText("Apellido: " + apellido);
+
+        TextView edadTextView = findViewById(R.id.edadTextView2);
+        edadTextView.setText("Edad: " + edad);
+
+        TextView telefonoTextView = findViewById(R.id.telefonoTextView2);
+        telefonoTextView.setText("Telefono: " + telefono);
+
+        TextView personaTextView = findViewById(R.id.personaTextView2);
+        personaTextView.setText("Usuario: " + correo);
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        Toast.makeText(miperfil.this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+}
